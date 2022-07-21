b394a7120b657bb5b07423931093503541a54bf5
MarkellBeast <km.benenson@gmail.com>
2022-07-21 13:36:32 +0300

    Добавьте файлы проекта.

---
+++ FinancialData_Handler/FinDataObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialData_ExcelHandler
+{
+    public class FinDataObject
+    {
+        //public int Id { get; set; }
+        public string Id { get; set; } // Int
+        public string Segment { get; set; }
+        public string Country { get; set; }
+        public string Product { get; set; }
+        public string DiscountBand { get; set; }
+
+        //public float UnitsSold { get; set; }
+        //public decimal ManufacturingPrice { get; set; }
+        //public decimal SalePrice { get; set; }
+        //public decimal GrossSales { get; set; }
+        //public decimal Discounts { get; set; }
+        //public decimal Sales { get; set; }
+        //public decimal COGS { get; set; }
+        //public decimal Profit { get; set; }
+        public string UnitsSold { get; set; }
+        public string ManufacturingPrice { get; set; }
+        public string SalePrice { get; set; }
+        public string GrossSales { get; set; }
+        public string Discounts { get; set; }
+        public string Sales { get; set; }
+        public string COGS { get; set; }
+        public string Profit { get; set; }
+        public string Date { get; set; }
+        //public byte MonthNumber { get; set; }
+        public string MonthNumber { get; set; }
+        public string MonthName { get; set; }
+        //public short Year { get; set; }
+        public string Year { get; set; }
+    }
+}
---
+++ FinancialData_Handler/Program.cs
@@ -0,0 +1,15 @@
+﻿using FinancialData_ExcelHandler;
+class Program
+{
+    public static void Main()
+    {
+        string path = @"C:\Users\markell\Desktop\Телеком-Сервис ИТ\finExample.xlsx";
+        Excel excel = new Excel(path, 1);
+
+        var data = excel.ReadFile(default, default).Where(x => Int32.Parse(x.Profit) > 100000);
+        foreach (var item in data)
+        {
+            Console.WriteLine($"id : {item.Id} - Product : {item.Product} - Country : {item.Country} - Date : {item.Date} - Profit : {item.Profit}");
+        }
+    }    
+}
